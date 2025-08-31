1dc6a0608aedbce70959941ac1f502de29f22920
Gabriel Garcia <gabrielproaj@gmail.com>
2025-08-30 22:27:20 -0300

    Seeding da base de dados

--- src/main/java/com/devsuperior/dscommerce/entities/OrderItem.java
+++ src/main/java/com/devsuperior/dscommerce/entities/OrderItem.java
@@ -2,9 +2,10 @@ package com.devsuperior.dscommerce.entities;
 
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "tb_order_item")
 public class OrderItem {
     @EmbeddedId
     private OrderItemPk id = new OrderItemPk();
